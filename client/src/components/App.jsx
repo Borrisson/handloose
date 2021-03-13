04f9f011bdd4d67598a94281972412dc0bd8cd53
Luc <lucborris@gmail.com>
2021-03-13 12:34:11 -0500

    Add Proside bar to sidebar component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,43 +1,13 @@
 import "./styles/App.scss";
 import useApplicationData from "../hooks/useApplicationData";
-import {
-  ProSidebar,
-  Menu,
-  MenuItem,
-  SubMenu,
-  SidebarHeader,
-  SidebarFooter,
-  SidebarContent,
-} from "react-pro-sidebar";
+import SideBar from "./sidebar/Sidebar";
 
 const App = () => {
   const { state, dispatch } = useApplicationData();
   const userList = state.users.map((user) => (
     <li key={user.id}>{user.name}</li>
   ));
-  return (
-    <ProSidebar rtl={true} image="../../static/media/background.jpg">
-      <SidebarHeader>
-        {/**
-         *  You can add a header for the sidebar ex: logo
-         */}
-      </SidebarHeader>
-      <SidebarContent>
-        <Menu iconShape="square">
-          <MenuItem>Dashboard</MenuItem>
-          <SubMenu title="Components">
-            <MenuItem>Component 1</MenuItem>
-            <MenuItem>Component 2</MenuItem>
-          </SubMenu>
-        </Menu>
-      </SidebarContent>
-      <SidebarFooter>
-        {/**
-         *  You can add a footer for the sidebar ex: copyright
-         */}
-      </SidebarFooter>
-    </ProSidebar>
-  );
+  return <SideBar />;
 };
 
 export default App;
--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,23 +1,35 @@
-import Button from "react-bootstrap/Button";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
+import {
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+} from "react-pro-sidebar";
 
 export default function SideBar(props) {
-  if (props.currentUser) {
-    return (
-      <Button variant="danger" onClick={props.onLogout}>
-        Logout
-      </Button>
-    );
-  } else {
-    return (
-      <ButtonGroup vertical>
-        <Button variant="primary" onClick={props.onRegister}>
-          Register
-        </Button>
-        <Button variant="primary" onClick={props.onLogin}>
-          Login
-        </Button>
-      </ButtonGroup>
-    );
-  }
+  return (
+    <ProSidebar rtl={true} image="../../static/media/background.jpg">
+      <SidebarHeader>
+        {/**
+         *  You can add a header for the sidebar ex: logo
+         */}
+      </SidebarHeader>
+      <SidebarContent>
+        <Menu iconShape="square">
+          <MenuItem>Dashboard</MenuItem>
+          <SubMenu title="Components">
+            <MenuItem>Component 1</MenuItem>
+            <MenuItem>Component 2</MenuItem>
+          </SubMenu>
+        </Menu>
+      </SidebarContent>
+      <SidebarFooter>
+        {/**
+         *  You can add a footer for the sidebar ex: copyright
+         */}
+      </SidebarFooter>
+    </ProSidebar>
+  );
 }
