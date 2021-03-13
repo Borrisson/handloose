19a3f98db798a3b5b73b165edfa11c2534bcbe58
Luc <lucborris@gmail.com>
2021-03-13 18:10:40 -0500

    Add register and login modals for form

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,14 @@
 import "./styles/App.scss";
 import useApplicationData from "../hooks/useApplicationData";
 import SideBar from "./sidebar/Sidebar";
+import Login from "./Login";
 
 const App = () => {
   const { state, dispatch } = useApplicationData();
   const userList = state.users.map((user) => (
     <li key={user.id}>{user.name}</li>
   ));
-  return <SideBar />;
+  return <Login />;
 };
 
 export default App;
