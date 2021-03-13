87ac30a11c0d1992393c59c0abb1581b8cba7dde
rafreza <rafintokyo@gmail.com>
2021-03-13 18:45:33 -0500

    migrate game screen

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,22 @@
 import "./styles/App.scss";
 import useApplicationData from "../hooks/useApplicationData";
 import SideBar from "./sidebar/Sidebar";
+import Phaser from 'phaser'
+import { IonPhaser } from '@ion-phaser/react'
 
+
+const game = {
+  width: "50px",
+  height: "50px",
+  type: Phaser.AUTO,
+  scene: {}
+}
 const App = () => {
   const { state, dispatch } = useApplicationData();
   const userList = state.users.map((user) => (
     <li key={user.id}>{user.name}</li>
   ));
-  return <SideBar />;
+  return (<><SideBar /> <IonPhaser game={game} /></>);
 };
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,18 @@
 import React from "react";
 import ReactDOM from 'react-dom'
-import { defineCustomElements as defineIonPhaser } from '@ion-phaser/core/loader'
-import Phaser from 'phaser'
 import "./index.scss";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
-const game = {
-  width: "100%",
-  height: "100%",
-  type: Phaser.AUTO,
-  scene: {}
-}
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <ion-phaser ref={el => el.game = game} />
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-defineIonPhaser(window);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
