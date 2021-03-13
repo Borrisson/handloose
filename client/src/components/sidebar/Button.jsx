357fe05dc46dd031ced1c606dede7a677c0845d0
Luc <lucborris@gmail.com>
2021-03-13 10:20:09 -0500

    Add bootstrap button

--- client/src/components/sidebar/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function Button(props) {
-  return <a {...props}>{props.label}</a>;
-}
-Button.defaultProps = {
-  onClick: undefined,
-};
--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,15 +1,15 @@
 import React, { Fragment } from "react";
-import Button from "./Button";
+import Button from "react-bootstrap/Button";
 
 export default function SideBar(props) {
   if (props.currentUser) {
-    return <Button label="Logout" onClick={props.onLogout} />;
+    return <Button onClick={props.onLogout}>Logout</Button>;
   } else {
     return (
       <>
-        <Button label="Register" onClick={props.onCreateAccount} />
+        <Button onClick={props.onRegister}>Register</Button>
         <br />
-        <Button label="Login" onClick={props.onLogin} />
+        <Button onClick={props.onLogin}>Login</Button>
       </>
     );
   }
