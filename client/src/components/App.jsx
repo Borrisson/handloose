28b51f648b28c1f18217c469689d3a48e6469afa
rafreza <rafintokyo@gmail.com>
2021-03-14 11:07:28 -0400

    get logo sprite to animate in game

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,11 +5,43 @@ import Phaser from 'phaser'
 import { IonPhaser } from '@ion-phaser/react'
 
 
+
+
 const game = {
-  width: "50px",
-  height: "50px",
+  width: "100%",
+  height: "100%",
   type: Phaser.AUTO,
-  scene: {}
+  pixelArt: true,
+  scene: {
+    preload: function() {
+      this.load.spritesheet('logo', 'assets/logo.png', {frameWidth: 291, frameHeight: 35})
+    },
+    create: function() {
+      /* this.helloWorld = this.add.text(
+        this.cameras.main.centerX, 
+        this.cameras.main.centerY, 
+        "Hello World", { 
+          font: "40px Arial", 
+          fill: "#ffffff" 
+        }
+      ); */
+      // this.helloWorld.setOrigin(0.5);
+      
+      this.anims.create({
+        key: 'logo',
+        frames: this.anims.generateFrameNumbers('logo'),
+        frameRate: 10,
+        repeat: -1
+      });
+      
+      const logo = this.add.sprite(720, 350);
+      logo.setScale(2);
+      logo.play('logo');
+    },
+    // update: function() {
+    //   this.helloWorld.angle += 1;
+    // }
+  }
 }
 const App = () => {
   const { state, dispatch } = useApplicationData();
