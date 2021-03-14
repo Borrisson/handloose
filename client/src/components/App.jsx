8c16e7ab6e98ed11da026a851c4c81876b8e1f29
rafreza <rafintokyo@gmail.com>
2021-03-14 11:39:54 -0400

    Move game scripts into Game component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,48 +2,13 @@ import "./styles/App.scss";
 import useApplicationData from "../hooks/useApplicationData";
 import SideBar from "./sidebar/Sidebar";
 import Login from "./Login";
-import Phaser from "phaser";
-import { IonPhaser } from "@ion-phaser/react";
+import Game from "./Game"
 
 
 
 
-const game = {
-  width: "100%",
-  height: "100%",
-  type: Phaser.AUTO,
-  pixelArt: true,
-  scene: {
-    preload: function() {
-      this.load.spritesheet('logo', 'assets/logo.png', {frameWidth: 291, frameHeight: 35})
-    },
-    create: function() {
-      /* this.helloWorld = this.add.text(
-        this.cameras.main.centerX, 
-        this.cameras.main.centerY, 
-        "Hello World", { 
-          font: "40px Arial", 
-          fill: "#ffffff" 
-        }
-      ); */
-      // this.helloWorld.setOrigin(0.5);
-      
-      this.anims.create({
-        key: 'logo',
-        frames: this.anims.generateFrameNumbers('logo'),
-        frameRate: 10,
-        repeat: -1
-      });
-      
-      const logo = this.add.sprite(720, 350);
-      logo.setScale(2);
-      logo.play('logo');
-    },
-    // update: function() {
-    //   this.helloWorld.angle += 1;
-    // }
-  }
-}
+
+
 const App = () => {
   const { state, dispatch } = useApplicationData();
   const userList = state.users.map((user) => (
@@ -54,7 +19,7 @@ const App = () => {
   return (
     <>
       <SideBar />
-      <IonPhaser game={game} />
+      <Game />
     </>
   );
 };
--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -1,3 +1,35 @@
-function Game() {
-  return <aside></aside>;
+import Phaser from "phaser";
+import { IonPhaser } from "@ion-phaser/react";
+
+
+export default function Game() {
+  const game = {
+    width: "100%",
+    height: "100%",
+    type: Phaser.AUTO,
+    pixelArt: true,
+    scene: {
+      preload: function() {
+        this.load.spritesheet('logo', 'assets/logo.png', {frameWidth: 291, frameHeight: 35})
+      },
+      create: function() {
+        
+        this.anims.create({
+          key: 'logo',
+          frames: this.anims.generateFrameNumbers('logo'),
+          frameRate: 10,
+          repeat: -1
+        });
+        
+        const logo = this.add.sprite(720, 350);
+        logo.setScale(2);
+        logo.play('logo');
+      },
+      
+    }
+  }
+  return (
+    <IonPhaser game={game} />
+  )
+
 }
