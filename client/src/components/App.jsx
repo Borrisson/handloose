9bfbf10f350d05db13cecb6f3e80184386e25633
Luc <lucborris@gmail.com>
2021-03-13 19:50:12 -0500

    Add jest-canvas-mock

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,17 +5,19 @@ import Login from "./Login";
 import Phaser from "phaser";
 import { IonPhaser } from "@ion-phaser/react";
 
-const game = {
-  width: "50px",
-  height: "50px",
-  type: Phaser.AUTO,
-  scene: {},
-};
 const App = () => {
   const { state, dispatch } = useApplicationData();
   const userList = state.users.map((user) => (
     <li key={user.id}>{user.name}</li>
   ));
+
+  const game = {
+    width: "50px",
+    height: "50px",
+    type: Phaser.AUTO,
+    scene: {},
+  };
+
   return (
     <>
       <IonPhaser game={game} />
