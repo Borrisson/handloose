7ec7ef0f63df120c27ff3ff5ae96b216c0ddff5e
Anto Ter-Mesropian <antotm99@gmail.com>
2021-03-15 23:31:24 +0000

    Fixing registration

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -10,8 +10,22 @@ class Api::UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
-    render json: @user
+    user= User.create!(
+      name: params['user']['name'],
+      email: params['user']['email'],
+      password: params['user']['password'],
+      password_confirmation: params['user']['password_confirmation']
+    )
+
+    if user
+      session[:user_id] = user.id
+      render json: {
+        status: :created,
+        user: user
+    }
+  else
+    render json: { status: 500 }
+    end
   end
 
   def update
