7450253fb21b8f6acf4e362c19fea5522638ef87
Luc <lucborris@gmail.com>
2021-03-15 10:20:15 -0400

    refactor test

--- client/src/hooks/__tests__/useInputData.test.js
+++ client/src/hooks/__tests__/useInputData.test.js
@@ -1,15 +1,16 @@
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook, act, cleanup } from "@testing-library/react-hooks";
 import useInputData from "../useInputData";
+afterEach(cleanup);
 describe("useInputData", () => {
+  const { result } = renderHook(() =>
+    useInputData({ email: "", password: "" })
+  );
   it("should initialize with empty string when passed no initial state", () => {
     const { result } = renderHook(() => useInputData());
     expect(result.current.input).toEqual("");
   });
 
   it("should initialize when passed an initial state", () => {
-    const { result } = renderHook(() =>
-      useInputData({ email: "", password: "" })
-    );
     expect(result.current.input).toEqual({ email: "", password: "" });
     expect(result.current.input).not.toEqual({ email: "" });
   });
