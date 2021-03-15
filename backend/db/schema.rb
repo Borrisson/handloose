d002bf2827bc053170732c0891ced5f4bb6c58ba
Borrisson <lucborris@gmail.com>
2021-03-15 20:33:06 +0000

    Add user_id to games table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_202754) do
+ActiveRecord::Schema.define(version: 2021_03_15_203140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_03_15_202754) do
     t.integer "longest_streak"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_games_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +43,5 @@ ActiveRecord::Schema.define(version: 2021_03_15_202754) do
   end
 
   add_foreign_key "accuracies", "games"
+  add_foreign_key "games", "users"
 end
