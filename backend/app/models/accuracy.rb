2dd29b67632198e1497ed8c526f0f9a18f002dce
Borrisson <lucborris@gmail.com>
2021-03-15 21:14:39 +0000

    rm accuracy model

--- backend/app/models/accuracy.rb
+++
@@ -1,5 +0,0 @@
-class Accuracy < ApplicationRecord
-  belongs_to :game
-
-  validates :character, length: { is: 1 }
-end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -13,8 +13,11 @@ require("faker")
 
   3.times do
     game = user.games.create!(score: Faker::Number.number(digits: 5), key_stroke_frequency: Faker::Number.number(digits: 2), longest_streak: Faker::Number.decimal_part(digits: 2))
-    140.times do
-      game.accuracies.create!(character: Faker::String.random(length: 1), hit: Faker::Boolean.boolean)
-    end
   end
+end
+
+game = user.games.create!(score: Faker::Number.number(digits: 5), key_stroke_frequency: Faker::Number.number(digits: 2), longest_streak: Faker::Number.decimal_part(digits: 2))
+
+140.times do
+  game.accuracies.create!(character: Faker::String.random(length: 1), hit: Faker::Boolean.boolean)
 end
