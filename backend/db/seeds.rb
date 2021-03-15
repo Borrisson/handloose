86acec6a93a87c2a5ea220571e9106d0c5ff054b
Borrisson <lucborris@gmail.com>
2021-03-15 20:51:00 +0000

    Add seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -9,5 +9,12 @@
 require("faker")
 
 3.times do
-  User.create(name: Faker::Name.first_name, email: Faker::Internet.email, password: "123456", password_confirmation: "123456")
+  user = User.create(name: Faker::Name.first_name, email: Faker::Internet.email, password: "123456", password_confirmation: "123456")
+
+  3.times do
+    game = user.games.create!(score: Faker::Number.number(digits: 5), key_stroke_frequency: Faker::Number.number(digits: 2), longest_streak: Faker::Number.decimal_part(digits: 2))
+    140.times do
+      game.accuracies.create!(character: Faker::String.random(length: 1), hit: Faker::Boolean.boolean)
+    end
+  end
 end
