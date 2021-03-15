4b53e7c1317dd8555e86a53a4559acbed0d9cf70
Borrisson <lucborris@gmail.com>
2021-03-15 21:35:31 +0000

    Remove example code folder

--- backend/PLACEHOLDER/20210315202754_add_game_id_to_accuracies.rb
+++
@@ -1,5 +0,0 @@
-class AddGameIdToAccuracies < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :accuracies, :game, index: true, foreign_key: true
-  end
-end
--- backend/PLACEHOLDER/20210315211449_create_accuracies.rb
+++
@@ -1,7 +0,0 @@
-class CreateAccuracies < ActiveRecord::Migration[6.1]
-  def change
-    create_table :accuracies do |t|
-      t.timestamps
-    end
-  end
-end
