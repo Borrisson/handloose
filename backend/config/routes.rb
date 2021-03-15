3f2267994315683b60a10a46ac525b0876fd2f2c
Anto Ter-Mesropian <antotm99@gmail.com>
2021-03-15 19:00:55 +0000

    added routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    resources :users
+    resources :users, except: [:destroy, :update]
+    resources :sessions, only: [:create, :destroy]
+    resources :games, except: [:destroy, :update]
   end
+
 
 end
