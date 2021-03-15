5199c1c9f218832c93d685a080370440436203f8
Borrisson <lucborris@gmail.com>
2021-03-15 21:52:56 +0000

    Fix Faker seeds and Fix accuracy presense true validations

--- backend/app/models/accuracy.rb
+++ backend/app/models/accuracy.rb
@@ -1,6 +1,5 @@
 class Accuracy < ApplicationRecord
   belongs_to :game
 
-  validates :character, length: { is: 1 }, presence: true
-  validates :hit, presence: true
+  validates :character, length: { is: 1 }
 end
