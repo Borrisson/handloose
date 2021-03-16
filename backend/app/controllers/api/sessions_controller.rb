85014cc4821b034c6aa0380874ee4016d7cfddeb
Luc <lucborris@gmail.com>
2021-03-16 13:33:13 -0400

    add class method of authenticate_with_credentials

--- backend/app/controllers/api/sessions_controller.rb
+++ backend/app/controllers/api/sessions_controller.rb
@@ -1,15 +1,11 @@
 class Api::SessionsController < ApplicationController
   def create
-    user = User
-      .find_by(email: params["user"]["email"])
-      .try(:authenticate, params["user"]["password"])
-
-    if user
-      session[:user_id] = user.id
+    if @user = User.authenticate_with_credentials(params[:email], params[:password])
+      session[:user_id] = @user.id
       render json: {
         status: :created,
         logged_in: "true",
-        user: user.name,
+        user: @user.name,
       }
     else
       render json: { status: 401 }
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   validates :email, { presence: true, uniqueness: { case_sensitive: false } }
   validates :password, { presence: true, confirmation: true, length: { in: 6..20 } }
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    User.find_by_email(email).try(:authenticate, password)
+  end
 end
