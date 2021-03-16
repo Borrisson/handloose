4a67763cb786940e681f2a499789765c6a89c92f
Anto Ter-Mesropian <antotm99@gmail.com>
2021-03-16 00:52:40 +0000

    Fixed sessions controller

--- backend/app/controllers/api/sessions_controller.rb
+++ backend/app/controllers/api/sessions_controller.rb
@@ -1,7 +1,20 @@
 class SessionsController < ApplicationController
 
   def create
-    session[:current_user_id] = @user.id
+    user = User
+            .find_by(email: params["user"]["email"])
+            .try(:authenticate, params["user"]["password"])
+
+    if user 
+      session[:user_id] = user_id
+      render json: {
+        status: :created
+        logged_in: true,
+        user: user
+      }
+    else
+      render json: { status: 401 }
+    end
   end
 
   def destroy
