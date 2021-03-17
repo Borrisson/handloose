76ae1c486eb268f26453baf22ef5e3ab7e469ba0
Borrisson <lucborris@gmail.com>
2021-03-17 15:17:21 +0000

    Remove show routes don't need individual hits or misses datta

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :users, except: [:destroy, :update] do
-      resources :games, except: [:destroy, :update] do
-        resources :accuracies, except: [:destroy, :update]
+      resources :games, except: [:destroy, :update, :show] do
+        resources :accuracies, except: [:destroy, :update, :show]
       end
     end
     resources :sessions, only: [:create, :destroy]
