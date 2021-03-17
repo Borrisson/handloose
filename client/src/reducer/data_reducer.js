9c3bde3e9eccbc0093af85fd284fb5e005158b51
Borrisson <lucborris@gmail.com>
2021-03-17 18:37:35 +0000

    Add switch case inside reducer for APP DATA

--- client/src/reducer/data_reducer.js
+++ client/src/reducer/data_reducer.js
@@ -1,8 +1,16 @@
+export const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
 export const SET_USER = "SET_USER";
 export const DESTROY_USER = "DESTROY_USER";
 
-const dataReducer = (state, { type, user }) => {
+const dataReducer = (state, { type, user, games, accuracies }) => {
   switch (type) {
+    case SET_APPLICATION_DATA:
+      return {
+        ...state,
+        user,
+        games,
+        accuracies,
+      };
     case SET_USER:
       return {
         ...state,
