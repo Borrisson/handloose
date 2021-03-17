4d2014c1a4d78b3b13651b9c797bd595bd0a49e6
Luc <lucborris@gmail.com>
2021-03-17 12:26:44 -0400

    Add react-router-dom to fix endpoint handling for now only redirects to homepage

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,32 @@
 import React from "react";
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
-import Game from "./components/Game"
-
-
-
+import Game from "./components/Game";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Game />
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Game />
+          <App />
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
