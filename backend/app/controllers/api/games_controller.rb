184c4b941f19dd87f8d90518fffe003a40588ffa
Luc <lucborris@gmail.com>
2021-03-17 11:05:08 -0400

    Add games for particular user

--- backend/app/controllers/api/games_controller.rb
+++ backend/app/controllers/api/games_controller.rb
@@ -1,6 +1,7 @@
 class Api::GamesController < ApplicationController
   def index
-    @games = Game.find params[:user_id]
+    @games = Game.where user_id: params[:user_id]
+    render json: @games
   end
 
   def create
