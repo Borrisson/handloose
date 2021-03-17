9fae2e391fb18238314608a9bd8adb1e3978919a
Luc <lucborris@gmail.com>
2021-03-17 10:53:21 -0400

    Reconfigure routes to make more RESTful sense

--- backend/app/controllers/api/games_controller.rb
+++ backend/app/controllers/api/games_controller.rb
@@ -1,7 +1,15 @@
 class Api::GamesController < ApplicationController
+  def index
+    @games = Game.find params[:user_id]
+  end
 
   def create
     @game = Game.create(game_params)
   end
 
+  private
+
+  def game_params
+    params.require(:game).permit(:score, :key_stroke_frequency, :longest_streak, :user_id)
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    resources :users, except: [:destroy, :update]
+    resources :users, except: [:destroy, :update] do
+      resources :games, except: [:destroy, :update]
+    end
     resources :sessions, only: [:create, :destroy]
-    resources :games, except: [:destroy, :update]
   end
-
-
 end
