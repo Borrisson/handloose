026fab73c6f1ffab3c785740b0358ca010d08235
Anto Ter-Mesropian <antotm99@gmail.com>
2021-03-18 19:40:32 +0000

    made game width 100%

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -32,7 +32,7 @@ export default class Game extends React.Component {
       // pixelArt: true,
       // scene: [Title, Menu, Levels]}) 
     const config = {
-      width: "80%",
+      width: "100%",
       height: "100%",
       type: Phaser.AUTO,
       parent: document.getElementById('phaser-game'),
