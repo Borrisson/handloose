9668cfe60bbce9fabf43bbd3ee16c3eb13e4b605
Luc <lucborris@gmail.com>
2021-03-19 12:27:30 -0400

    Add modal to leaderboard

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -1,21 +1,35 @@
 import Table from "react-bootstrap/Table";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import LeaderboardItem from "./LeaderboardItem";
 
-export default function Leaderboard(props) {
-  const parsedTable = props.leaderboard.map((el) => {
+export default function Leaderboard({ handleClose, show, leaderboard }) {
+  const parsedTable = leaderboard.map((el) => {
     <LeaderboardItem key={el.id} {...el} />;
   });
   return (
-    <Table striped bordered hover variant="dark" responsive>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>User</th>
-          <th>Score</th>
-          <th>Streak</th>
-        </tr>
-      </thead>
-      <tbody>{parsedTable}</tbody>
-    </Table>
+    <Modal.Dialog>
+      <Modal.Header closeButton>
+        <Modal.Title>Leaderboard</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <Table striped bordered hover variant="dark" responsive>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>User</th>
+              <th>Score</th>
+              <th>Streak</th>
+            </tr>
+          </thead>
+          <tbody>{parsedTable}</tbody>
+        </Table>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary">Close</Button>
+        <Button variant="primary">Save changes</Button>
+      </Modal.Footer>
+    </Modal.Dialog>
   );
 }
