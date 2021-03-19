68345c1314de3cf897d5e584b0572d5c08b01c1a
Luc <lucborris@gmail.com>
2021-03-19 11:40:46 -0400

    Add query for leaderboard

--- backend/app/controllers/api/games_controller.rb
+++ backend/app/controllers/api/games_controller.rb
@@ -1,10 +1,14 @@
 class Api::GamesController < ApplicationController
+  skip_before_action :require_login, only: [:create]
+
   def index
     @games
     @accuracies
     if params[:user_id]
       @games = Game.where(user_id: params[:user_id]).order("score DESC").limit(10)
       @accuracies = Accuracy.where(game_id: @games.ids)
+    elsif required_query_params
+      @games = Game.joins(:user).select("games.*, users.name").order("#{game_query[:order_by]} desc").limit(game_query[:limit].to_i)
     else
       @games = Game.all
       @accuracies = Accuracy.all
@@ -24,5 +28,13 @@ class Api::GamesController < ApplicationController
 
   def game_params
     params.require(:game).permit(:score, :key_stroke_frequency, :longest_streak, :user_id)
+  end
+
+  def game_query
+    params.permit(:score, :key_stroke_frequency, :longest_streak, :user_id, :order_by, :direction, :limit)
+  end
+
+  def required_query_params
+    request.query_parameters[:order_by] && request.query_parameters[:limit]
   end
 end
