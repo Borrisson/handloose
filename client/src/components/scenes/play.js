faae13e37ac1ac5c4850b391cd4ecbe7d715a625
Luc <lucborris@gmail.com>
2021-03-19 17:21:31 -0400

    Add sprite creation height location

--- client/src/components/scenes/play.js
+++ client/src/components/scenes/play.js
@@ -1,4 +1,4 @@
-import Phaser from "phaser"
+import Phaser from "phaser";
 
 function checkOverlap(spriteA, spriteB) {
   let boundsA = spriteA.getBounds();
@@ -7,62 +7,104 @@ function checkOverlap(spriteA, spriteB) {
   return Phaser.Geom.Intersects.RectangleToRectangle(boundsA, boundsB);
 }
 
-
-
-
-
-
-const position = [3.34, 3.23, 3.13, 621, 609, 693, 765, 838, 982, 910, 982, 1053, 921, 849, 1042, 1114, 460, 682, 550, 755, 898, 705, 538, 561, 826, 490]
+const position = [
+  3.34,
+  3.23,
+  3.13,
+  621,
+  609,
+  693,
+  765,
+  838,
+  982,
+  910,
+  982,
+  1053,
+  921,
+  849,
+  1042,
+  1114,
+  460,
+  682,
+  550,
+  755,
+  898,
+  705,
+  538,
+  561,
+  826,
+  490,
+];
 
 export default class Play extends Phaser.Scene {
   constructor() {
-    super("play")
+    super("play");
   }
   preload() {
-    this.load.audio('main_theme', 'assets/audio/main_theme.mp3')
-    this.load.spritesheet('kb1', 'assets/kb1.png', {frameWidth: 119, frameHeight: 10});
-    this.load.spritesheet('kb2', 'assets/kb2.png', {frameWidth: 107, frameHeight: 11});
-    this.load.spritesheet('kb3', 'assets/kb3.png', {frameWidth: 83, frameHeight: 12});
-    this.load.spritesheet('text', 'assets/text.png', {frameWidth: 7, frameHeight: 8})
+    this.load.audio("main_theme", "assets/audio/main_theme.mp3");
+    this.load.spritesheet("kb1", "assets/kb1.png", {
+      frameWidth: 119,
+      frameHeight: 10,
+    });
+    this.load.spritesheet("kb2", "assets/kb2.png", {
+      frameWidth: 107,
+      frameHeight: 11,
+    });
+    this.load.spritesheet("kb3", "assets/kb3.png", {
+      frameWidth: 83,
+      frameHeight: 12,
+    });
+    this.load.spritesheet("text", "assets/text.png", {
+      frameWidth: 7,
+      frameHeight: 8,
+    });
   }
   getLetter() {
-    const num = (Math.floor(Math.random() * (3 - 0) ) + 0);
-    let posX = position[1]
-    this.letter = this.physics.add.sprite(this.scale.width / posX, 1000, 'text', 1)
-    this.letter.setScale(6).setVelocityY(-(window.velocity));
+    const num = Math.floor(Math.random() * (3 - 0)) + 0;
+    let posX = position[1];
+    console.log(this.physics);
+    this.letter = this.physics.add.sprite(
+      this.scale.width / posX,
+      this.scale.height,
+      "text",
+      1
+    );
+    this.letter.setScale(6).setVelocityY(-window.velocity);
   }
   create() {
-    this.key_SPACE = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.SPACE)
-    this.key_A = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.A)
-    this.key_B = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.B)
-    this.key_C = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.C)
-    this.key_D = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.D)
-    this.key_E = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.E)
-    this.key_F = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.F)
-    this.key_G = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.G)
-    this.key_H = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.H)
-    this.key_I = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.I)
-    this.key_J = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.J)
-    this.key_K = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.K)
-    this.key_L = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.L)
-    this.key_M = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.M)
-    this.key_N = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.N)
-    this.key_O = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.O)
-    this.key_P = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.P)
-    this.key_Q = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.Q)
-    this.key_R = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.R)
-    this.key_S = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.S)
-    this.key_T = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.T)
-    this.key_U = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.U)
-    this.key_V = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.V)
-    this.key_W = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.W)
-    this.key_X = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.X)
-    this.key_Y = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.Y)
-    this.key_Z = this.input.keyboard.addKey (Phaser.Input.Keyboard.KeyCodes.Z)
+    this.key_SPACE = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
+    this.key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    this.key_B = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.B);
+    this.key_C = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
+    this.key_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    this.key_E = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
+    this.key_F = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+    this.key_G = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.G);
+    this.key_H = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.H);
+    this.key_I = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.I);
+    this.key_J = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.J);
+    this.key_K = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.K);
+    this.key_L = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.L);
+    this.key_M = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M);
+    this.key_N = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.N);
+    this.key_O = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.O);
+    this.key_P = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
+    this.key_Q = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
+    this.key_R = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+    this.key_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    this.key_T = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.T);
+    this.key_U = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.U);
+    this.key_V = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.V);
+    this.key_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    this.key_X = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
+    this.key_Y = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Y);
+    this.key_Z = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
     this.pausePhysics = false;
-    
-    this.music = this.sound.add('main_theme');
-    
+
+    this.music = this.sound.add("main_theme");
+
     let musicConfig = {
       mute: false,
       volume: 1,
@@ -70,210 +112,204 @@ export default class Play extends Phaser.Scene {
       detune: 0,
       seek: 0,
       loop: true,
-      delay: 0
-    }
+      delay: 0,
+    };
 
     this.music.play(musicConfig);
 
-    
     this.kb1 = this.add
-      .sprite(this.scale.width / 1.945, this.scale.height / 5.05, 'kb1')
-      .setScale(6)
+      .sprite(this.scale.width / 1.945, this.scale.height / 5.05, "kb1")
+      .setScale(6);
     this.kb2 = this.add
-      .sprite(this.scale.width / 2.01, this.scale.height / 3.9, 'kb2')
+      .sprite(this.scale.width / 2.01, this.scale.height / 3.9, "kb2")
       .setScale(6);
     this.kb3 = this.add
-      .sprite(this.scale.width / 2.175, this.scale.height / 3.1, 'kb3')
+      .sprite(this.scale.width / 2.175, this.scale.height / 3.1, "kb3")
       .setScale(6);
-    
-    this.input.keyboard.on('keydown-A', () => {
-     this.kb2.setFrame(1);
-    })
-    this.input.keyboard.on('keyup-A', () => {
+
+    this.input.keyboard.on("keydown-A", () => {
+      this.kb2.setFrame(1);
+    });
+    this.input.keyboard.on("keyup-A", () => {
       this.kb2.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-B', () => {
-      this.kb3.setFrame(5); 
-     })
-    this.input.keyboard.on('keyup-B', () => {
+    });
+    this.input.keyboard.on("keydown-B", () => {
+      this.kb3.setFrame(5);
+    });
+    this.input.keyboard.on("keyup-B", () => {
       this.kb3.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-C', () => {
-      this.kb3.setFrame(3); 
-    })
-    this.input.keyboard.on('keyup-C', () => {
+    });
+    this.input.keyboard.on("keydown-C", () => {
+      this.kb3.setFrame(3);
+    });
+    this.input.keyboard.on("keyup-C", () => {
       this.kb3.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-D', () => {
-      this.kb2.setFrame(3); 
-    })
-    this.input.keyboard.on('keyup-D', () => {
+    });
+    this.input.keyboard.on("keydown-D", () => {
+      this.kb2.setFrame(3);
+    });
+    this.input.keyboard.on("keyup-D", () => {
       this.kb2.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-E', () => {
-      this.kb1.setFrame(3); 
-    })
-    this.input.keyboard.on('keyup-E', () => {
+    });
+    this.input.keyboard.on("keydown-E", () => {
+      this.kb1.setFrame(3);
+    });
+    this.input.keyboard.on("keyup-E", () => {
       this.kb1.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-F', () => {
-      this.kb2.setFrame(4); 
-    })
-    this.input.keyboard.on('keyup-F', () => {
+    });
+    this.input.keyboard.on("keydown-F", () => {
+      this.kb2.setFrame(4);
+    });
+    this.input.keyboard.on("keyup-F", () => {
       this.kb2.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-G', () => {
-      this.kb2.setFrame(5); 
-    })
-    this.input.keyboard.on('keyup-G', () => {
+    });
+    this.input.keyboard.on("keydown-G", () => {
+      this.kb2.setFrame(5);
+    });
+    this.input.keyboard.on("keyup-G", () => {
       this.kb2.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-H', () => {
-      this.kb2.setFrame(6); 
-    })
-    this.input.keyboard.on('keyup-H', () => {
+    });
+    this.input.keyboard.on("keydown-H", () => {
+      this.kb2.setFrame(6);
+    });
+    this.input.keyboard.on("keyup-H", () => {
       this.kb2.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-I', () => {
-      this.kb1.setFrame(8); 
-    })
-    this.input.keyboard.on('keyup-I', () => {
+    });
+    this.input.keyboard.on("keydown-I", () => {
+      this.kb1.setFrame(8);
+    });
+    this.input.keyboard.on("keyup-I", () => {
       this.kb1.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-J', () => {
-      this.kb2.setFrame(7); 
-    })
-    this.input.keyboard.on('keyup-J', () => {
+    });
+    this.input.keyboard.on("keydown-J", () => {
+      this.kb2.setFrame(7);
+    });
+    this.input.keyboard.on("keyup-J", () => {
       this.kb2.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-K', () => {
-      this.kb2.setFrame(8); 
-    })
-    this.input.keyboard.on('keyup-K', () => {
+    });
+    this.input.keyboard.on("keydown-K", () => {
+      this.kb2.setFrame(8);
+    });
+    this.input.keyboard.on("keyup-K", () => {
       this.kb2.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-L', () => {
-      this.kb2.setFrame(9); 
-    })
-    this.input.keyboard.on('keyup-L', () => {
+    });
+    this.input.keyboard.on("keydown-L", () => {
+      this.kb2.setFrame(9);
+    });
+    this.input.keyboard.on("keyup-L", () => {
       this.kb2.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-M', () => {
-      this.kb3.setFrame(7); 
-    })
-    this.input.keyboard.on('keyup-M', () => {
+    });
+    this.input.keyboard.on("keydown-M", () => {
+      this.kb3.setFrame(7);
+    });
+    this.input.keyboard.on("keyup-M", () => {
       this.kb3.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-N', () => {
-      this.kb3.setFrame(6); 
-    })
-    this.input.keyboard.on('keyup-N', () => {
+    });
+    this.input.keyboard.on("keydown-N", () => {
+      this.kb3.setFrame(6);
+    });
+    this.input.keyboard.on("keyup-N", () => {
       this.kb3.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-O', () => {
-      this.kb1.setFrame(9); 
-    })
-    this.input.keyboard.on('keyup-O', () => {
+    });
+    this.input.keyboard.on("keydown-O", () => {
+      this.kb1.setFrame(9);
+    });
+    this.input.keyboard.on("keyup-O", () => {
       this.kb1.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-P', () => {
-      this.kb1.setFrame(10); 
-    })
-    this.input.keyboard.on('keyup-P', () => {
+    });
+    this.input.keyboard.on("keydown-P", () => {
+      this.kb1.setFrame(10);
+    });
+    this.input.keyboard.on("keyup-P", () => {
       this.kb1.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-Q', () => {
-      this.kb1.setFrame(1); 
-    })
-    this.input.keyboard.on('keyup-Q', () => {
+    });
+    this.input.keyboard.on("keydown-Q", () => {
+      this.kb1.setFrame(1);
+    });
+    this.input.keyboard.on("keyup-Q", () => {
       this.kb1.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-R', () => {
-      this.kb1.setFrame(4); 
-    })
-    this.input.keyboard.on('keyup-R', () => {
+    });
+    this.input.keyboard.on("keydown-R", () => {
+      this.kb1.setFrame(4);
+    });
+    this.input.keyboard.on("keyup-R", () => {
       this.kb1.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-S', () => {
-      this.kb2.setFrame(2); 
-    })
-    this.input.keyboard.on('keyup-S', () => {
+    });
+    this.input.keyboard.on("keydown-S", () => {
+      this.kb2.setFrame(2);
+    });
+    this.input.keyboard.on("keyup-S", () => {
       this.kb2.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-T', () => {
-      this.kb1.setFrame(5); 
-    })
-    this.input.keyboard.on('keyup-T', () => {
+    });
+    this.input.keyboard.on("keydown-T", () => {
+      this.kb1.setFrame(5);
+    });
+    this.input.keyboard.on("keyup-T", () => {
       this.kb1.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-U', () => {
-      this.kb1.setFrame(7); 
-    })
-    this.input.keyboard.on('keyup-U', () => {
+    });
+    this.input.keyboard.on("keydown-U", () => {
+      this.kb1.setFrame(7);
+    });
+    this.input.keyboard.on("keyup-U", () => {
       this.kb1.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-V', () => {
-      this.kb3.setFrame(4); 
-    })
-    this.input.keyboard.on('keyup-V', () => {
+    });
+    this.input.keyboard.on("keydown-V", () => {
+      this.kb3.setFrame(4);
+    });
+    this.input.keyboard.on("keyup-V", () => {
       this.kb3.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-W', () => {
-      this.kb1.setFrame(2); 
-    })
-    this.input.keyboard.on('keyup-W', () => {
+    });
+    this.input.keyboard.on("keydown-W", () => {
+      this.kb1.setFrame(2);
+    });
+    this.input.keyboard.on("keyup-W", () => {
       this.kb1.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-X', () => {
-      this.kb3.setFrame(2); 
-    })
-    this.input.keyboard.on('keyup-X', () => {
+    });
+    this.input.keyboard.on("keydown-X", () => {
+      this.kb3.setFrame(2);
+    });
+    this.input.keyboard.on("keyup-X", () => {
       this.kb3.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-Y', () => {
-      this.kb1.setFrame(6); 
-    })
-    this.input.keyboard.on('keyup-Y', () => {
+    });
+    this.input.keyboard.on("keydown-Y", () => {
+      this.kb1.setFrame(6);
+    });
+    this.input.keyboard.on("keyup-Y", () => {
       this.kb1.setFrame(0);
-    })
-    this.input.keyboard.on('keydown-Z', () => {
-      this.kb3.setFrame(1); 
-    })
-    this.input.keyboard.on('keyup-Z', () => {
+    });
+    this.input.keyboard.on("keydown-Z", () => {
+      this.kb3.setFrame(1);
+    });
+    this.input.keyboard.on("keyup-Z", () => {
       this.kb3.setFrame(0);
-    })
+    });
     // this.time.delayedCall(2000, onEvent, [], this);
     this.getLetter();
 
-    this.time.addEvent({ delay: 3000, loop: true, callback: this.getLetter, callbackScope: this});
+    this.time.addEvent({
+      delay: 3000,
+      loop: true,
+      callback: this.getLetter,
+      callbackScope: this,
+    });
     this.scale.on("resize", this.resize, this);
   }
 
-  
-  
-  
   update() {
-    
-      if (checkOverlap(this.letter, this.kb2)){
-        console.log('collide!');
-        if (this.key_A.isDown){
-          
-          this.letter.destroy();
-        }
+    if (checkOverlap(this.letter, this.kb2)) {
+      console.log("collide!");
+      if (this.key_A.isDown) {
+        this.letter.destroy();
       }
-    
-    
-    
+    }
 
-    if ( this.key_SPACE.isDown && this.pausePhysics === false) {
+    if (this.key_SPACE.isDown && this.pausePhysics === false) {
       this.pausePhysics = true;
       this.physics.pause();
-    } else if (this.pausePhysics === true && this.key_SPACE.isDown){
+    } else if (this.pausePhysics === true && this.key_SPACE.isDown) {
       this.pausePhysics = false;
       this.physics.resume();
     }
-
-  
   }
   resize(gameSize, baseSize, displaySize, resolution) {
     var width = gameSize.width;
@@ -283,10 +319,6 @@ export default class Play extends Phaser.Scene {
 
     this.kb1.setPosition(this.scale.width / 1.945, this.scale.height / 5.05);
     this.kb2.setPosition(this.scale.width / 2.01, this.scale.height / 3.9);
-    this.kb3.setPosition(
-      this.scale.width / 2.175,
-      this.scale.height / 3.1
-    );
-    
+    this.kb3.setPosition(this.scale.width / 2.175, this.scale.height / 3.1);
   }
 }
