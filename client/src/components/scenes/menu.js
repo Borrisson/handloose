25bbce282f9e618e96ffc0f593efd52491a4badf
Luc <lucborris@gmail.com>
2021-03-21 21:02:59 -0400

    Refactor menu to minimize variable calls by chaining instead

--- client/src/components/scenes/menu.js
+++ client/src/components/scenes/menu.js
@@ -34,8 +34,7 @@ export default class Menu extends Phaser.Scene {
       .setScale(9);
     this.curtainRight = this.add
       .image(this.scale.width / 1.1, this.scale.height / 3, "curtains")
-      .setScale(9);
-    this.curtainRight.flipX = true;
+      .setScale(9).flipX = true;
 
     this.anims.create({
       key: "logo",
