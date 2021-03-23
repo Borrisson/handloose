d2f447112adf2b8d8148e10c024d83054eee5f36
Luc <lucborris@gmail.com>
2021-03-23 10:01:51 -0400

    Remove commented out code from sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -34,22 +34,14 @@ export default function SideBar({ state, handleShow, handleLogout, score }) {
   });
 
   useEffect(() => {
-    // Handler to call on window resize
     function handleResize() {
-      // Set window width/height to state
       setWindowSize({
         width: window.innerWidth,
         height: window.innerHeight,
       });
     }
-
-    // Add event listener
     window.addEventListener("resize", handleResize);
-
-    // Call handler right away so state gets updated with initial window size
     handleResize();
-
-    // Remove event listener on cleanup
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
