d8c85be9445e107e2a7893a3d5c7820e48a41bfd
Luc <lucborris@gmail.com>
2021-03-23 12:45:46 -0400

    Fix seeds for realistic data for games

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -8,11 +8,13 @@
 
 require("faker")
 
-3.times do
+User.create(name: "Alice", email: "alice@wonderland.com", password: "password", password_confirmation: "password")
+
+10.times do
   user = User.create(name: Faker::Name.first_name, email: Faker::Internet.email, password: "123456", password_confirmation: "123456")
 
-  3.times do
-    game = user.games.create!(score: Faker::Number.number(digits: 5), key_stroke_frequency: Faker::Number.number(digits: 2), longest_streak: Faker::Number.decimal_part(digits: 2))
+  10.times do
+    game = user.games.create!(score: Faker::Number.within(range: 20000..232030), key_stroke_frequency: Faker::Number.number(digits: 2), longest_streak: Faker::Number.within(range: 1..140))
 
     140.times do
       game.accuracies.create!(character: Faker::Alphanumeric.alphanumeric(number: 1, min_alpha: 1), hit: Faker::Boolean.boolean)
